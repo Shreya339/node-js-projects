6065b730b98865a3a71f6b5390dc4a4c92832b62
Shreya .S <55738924+Shreya339@users.noreply.github.com>
2024-01-29 17:42:39 -0800

    Installing packages and gitignore

--- index.js
+++ index.js
@@ -1,24 +1,8 @@
-const fs = require('fs')
-const app = require('./app')
-// console.log(app.z())
-// const arr=[2,5,1,9,6,3,6,3,3];
-// let result = arr.filter((item)=> {
-//     return item == 3
-// })
-// console.log(result)
+import chalk from 'chalk';
+import colors from 'colors';
 
-// fs.writeFileSync("hello.txt","code step by step")
-// console.log("-->>",__dirname)
+// var clrs = require('colors')
+console.log("hello".red)
 
-const http = require('http');
-// http.createServer((req,res)=>{
-//     // req -> accesses sent parameters via url from inside node js
-//     res.write("Hello this is Shreya");
-//     res.end();
-// }).listen(4500);
-
-// const datacontrol = (req,res) => {
-//     res.write("Hello this is Shreya");npm init
-//     res.end();
-// }
-// http.createServer(datacontrol)
+// var chalk = require('chalk')
+console.log(chalk.blue("hello"))
