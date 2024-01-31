2c8595a6cdad5ba773f858b80cb85c8169fedd35
Shreya .S <55738924+Shreya339@users.noreply.github.com>
2024-01-31 02:04:57 -0800

    CRUD operations

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
-import chalk from 'chalk';
-import colors from 'colors';
+import * as fs from 'fs';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
 
-// var clrs = require('colors')
-console.log("hello".red)
+const __filename = fileURLToPath(import.meta.url); //getfile path
+const __dirname = path.dirname(__filename); //get directory path using filepath
+const filepath = path.join(__dirname, 'test_file.txt'); //create new file path
 
-// var chalk = require('chalk')
-console.log(chalk.blue("hello"))
+/* CRUD */
+// /* Create */
+fs.writeFileSync(filepath,'This is a test file');
+
+// /* Read */
+fs.readFile(filepath,'utf8',(err,item)=>{  //we add utf 8 for buffer encoding
+    console.log(item)
+})
+
+// /* Update */
+fs.appendFile(filepath,'for testing CRUD operations',(err)=>{
+    !err && console.log("File is updated");
+})
+
+// /* Rename */
+fs.rename(filepath,'renamed_test_file.txt',(err)=>{
+    !err && console.log("File is renamed");
+})
+
+/* Delete */
+fs.unlinkSync(`${__dirname}/renamed_test_file.txt`,(err)=>{
+    !err && console.log("File is deleted");
+})
