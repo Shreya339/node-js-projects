d3a121d171cc8ca370f1b328735d65edf8dd45ef
Shreya .S <55738924+Shreya339@users.noreply.github.com>
2024-02-02 17:53:31 -0800

    Application and Route level middleware

--- index.js
+++ index.js
@@ -1,8 +1,41 @@
-import chalk from 'chalk';
-import colors from 'colors';
+import express from 'express' ;
+const app = new express();
 
-// var clrs = require('colors')
-console.log("hello".red)
+app.listen('5000')
 
-// var chalk = require('chalk')
-console.log(chalk.blue("hello"))
+/* Creating a Application level Middleware- applied on all routes */  
+const reqFilter = (req,res,next)=>{
+    if(!req.query.age){
+        res.send("Please provide age")
+    } 
+    else if(req.query.age<18){
+        res.send("You cannot access this page")
+    }
+    else {
+        next();  //if this is not present,browser never finishes loading and not able to route
+    }
+}
+
+// ----------------------------------------------------------------------------------------
+
+/* Application level middleware-applies on all routes */
+//app.use(reqFilter)
+
+app.get('/',(req,res)=>{
+    res.send("Welcome to home page")
+})
+
+app.get('/user',(req,res)=>{
+    res.send("Welcome to user page")
+})
+
+// ------------------------------------------------------------------------------------
+
+/* Route level middleware reqFilter, Applies on only this specific route */
+
+app.get('/about',reqFilter,(req,res)=>{
+    res.send("Welcome to about page")
+})
+
+
+
