1b7401f814387aceb8bab596384fdad8120bdbb0
Shreya .S <55738924+Shreya339@users.noreply.github.com>
2024-02-03 18:55:02 -0800

    Connect NodeJs with MongoSB

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-import chalk from 'chalk';
-import colors from 'colors';
+import { MongoClient } from "mongodb";
 
-// var clrs = require('colors')
-console.log("hello".red)
+const url = "mongodb://0.0.0.0:27017";  //always same url for local mongodb
+const client = new MongoClient(url);
+const database = 'e-comm';
 
-// var chalk = require('chalk')
-console.log(chalk.blue("hello"))
+async function getData(){
+    let connection= await client.connect();  //returns a promise
+    let db = connection.db(database);
+    let collection = db.collection('product');
+    let response = await collection.find({}).toArray();
+    console.log(response);
+}
+
+getData();
